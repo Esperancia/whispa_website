2dbe12aefc34539efc8762d456f7cfaaa9f02a7e
Aymar Sossou <saymar90@gmail.com>
2015-07-19 21:36:26 +0100

    Added jQuery Validation Script to Inscription Form.

--- js/site/inscription.js
+++ js/site/inscription.js
@@ -1,5 +1,47 @@
-window.Parsley.on('form:success', function() {
-  $('#info-envoi').html("Votre inscription a été enregistrée. Nous vous contacterons bientôt.");
-  $('#inscription_form').hide();
+$("#inscription_form").validate({
+  rules: {
+    field: {
+      required: true,
+      number: true
+    }
+  }
 });
 
+$(document).ready(function(){
+  var $l = $('.langages');
+  var $cdigitales = $('.connaissancesdigitales');
+  var $cdesign = $('.connaissancesdesign');
+
+  $l.change(function(e){
+    if( $('#connaissance12').is(':checked') || $('#connaissance13').is(':checked')){
+      $('#langages').prop('required',true);
+    } else {
+      $('#langages').prop('required',false);
+    }
+  });
+
+  $cdigitales.change(function(e){
+    if( $('#connaissance24').is(':checked') ){
+      $('#connaissance2a').prop('required',true);
+    } else {
+      $('#connaissance2a').prop('required',false);
+    }
+  });
+
+  $cdesign.change(function(e){
+    if( $('#connaissance35').is(':checked') ){
+      $('#connaissance3a').prop('required',true);
+    } else {
+      $('#connaissance3a').prop('required',false);
+    }
+  });
+
+  $('#submit').click(function(){
+    if ($('#inscription_form').valid()) {
+      $('#inscription_form').submit(function(){
+        $('#info-envoi').html("Votre inscription a &eacute;t&eacute; enregistr&eacute;e. Nous vous contacterons bient&ocirc;t.");
+        $('#inscription_form').hide();
+      });
+    }
+  });
+});
